b4b41f587a07e336260e2663b1356512703e82b9
unixadmin <129633664+x86-unix@users.noreply.github.com>
2023-06-29 11:26:47 +0900

    Update init.el

--- init.el
+++ init.el
@@ -183,21 +183,9 @@
 )
 
 ;; Theme
-(use-package doom-themes
+(use-package zenburn-theme
   :config
-  ; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  ; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)  ; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (load-theme 'zenburn t))
 
 ;; rainbow-delimiters
 (use-package rainbow-delimiters
