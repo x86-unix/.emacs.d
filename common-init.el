1ec8029accf88616766ccb2c6626262756f098fc
unixadmin <y.ochiai@x86-unix.com>
2024-12-29 23:39:32 +0900

    update

--- common-init.el
+++ common-init.el
@@ -28,6 +28,7 @@
 ; Theme
 (use-package gruvbox-theme :config (load-theme 'gruvbox-dark-hard t))
 (set-face-foreground 'font-lock-comment-face "purple")
+
 ; nerd-iconsの設定
 (use-package nerd-icons
   :ensure t
@@ -35,17 +36,20 @@
   (unless (or (file-exists-p "~/NFM.ttf")
               (file-exists-p "~/.local/share/fonts/NFM.ttf"))
     (nerd-icons-install-fonts)))
+
 ; dired-sidebarの設定
 (use-package dired-sidebar
   :ensure t
   :bind (("C-c t" . dired-sidebar-toggle-sidebar))
   :config
   (setq dired-sidebar-theme 'nerd-icons))
+
 ; nerd-icons-diredの設定
 (use-package nerd-icons-dired
   :ensure t
   :after dired
   :hook (dired-sidebar-mode . nerd-icons-dired-mode))
+
 ; Other Settings
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -115,4 +119,3 @@
   (recenter))
 
 (global-set-key (kbd "C-c l") 'toggle-truncate-lines) ; ON/OFF
-
