ab089adac6a0df67c56f65fe958d2d1357f14df0
unixadmin <y.ochiai@x86-unix.com>
2024-12-30 13:08:56 +0900

    update

--- common-init.el
+++ common-init.el
@@ -56,6 +56,29 @@
 (setq-default indent-tabs-mode nil) ; スペースでインデント
 (display-time-mode t)
 
+; 空白等の表示
+(use-package whitespace
+  :ensure t
+  :hook ((prog-mode . whitespace-mode)
+         (text-mode . whitespace-mode))
+  :custom
+  (whitespace-style '(face
+                      tabs
+                      spaces
+                      newline
+                      space-mark
+                      tab-mark
+                      newline-mark))
+  (whitespace-space-regexp "\\(\u3000\\| \\)")
+  (whitespace-display-mappings
+   '((space-mark   ?\u3000 [?\u25A1] [?_]) ; 全角スペースを四角で表示
+     (space-mark   ?\s [?\u00B7])          ; 半角スペースを中点で表示
+     (newline-mark ?\n [?\u2193 ?\n])       ; 改行を下矢印で表示
+     (tab-mark     ?\t [?\u2192 ?\t])))     ; タブを矢印で表示
+  (whitespace-action '(auto-cleanup))
+  :config
+  (setq whitespace-global-modes '(not org-mode)))
+
 ;; バックアップ設定
 (setq make-backup-files nil)       ; バックアップファイルを無効化
 (setq auto-save-default nil)       ; 自動保存を無効化
