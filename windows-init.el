476b73e579c4bbd200b1449c09741739b5999ff7
unixadmin <y.ochiai@x86-unix.com>
2024-12-30 17:58:54 +0900

    update

--- windows-init.el
+++ windows-init.el
@@ -2,17 +2,12 @@
 ; shell-pop
 (use-package shell-pop
   :init
-  ;; Windows用のシェルの設定
   (setq shell-pop-shell-type
-        '("powershell"
-          "*PowerShell*"
-          (lambda () (powershell)))) ;; PowerShellを使用する場合
-  ;; (setq shell-pop-shell-type
-  ;;       '("cmd"
-  ;;         "*Command Prompt*"
-  ;;         (lambda () (ansi-term "cmd.exe")))) ;; cmdを使用する場合
+        '("shell"
+          "*Shell*"
+          (lambda () (shell))))  ;; shellを使用
   (setq shell-pop-full-span t)
-  (setq shell-pop-window-size 20) ;; Specify window size as 20% of screen height
+  (setq shell-pop-window-size 20) ;; ウィンドウサイズを20%に設定
   :bind (("C-c s" . shell-pop))
   :config
   (defun my/shell-mode-setup ()
@@ -21,4 +16,3 @@
     (define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
     (define-key shell-mode-map (kbd "C-n") 'comint-next-input))
   (add-hook 'shell-mode-hook 'my/shell-mode-setup))
-
