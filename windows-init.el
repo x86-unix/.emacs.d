84d9a127d2a1aa5dfa3e5636f8045fa4f6f02c2d
unixadmin <y.ochiai@x86-unix.com>
2024-12-30 18:12:57 +0900

    update

--- windows-init.el
+++ windows-init.el
@@ -1,5 +1,5 @@
 ;; shell
-; shell-pop
+; shell-popの設定
 (use-package shell-pop
   :init
   (setq shell-pop-shell-type
@@ -14,5 +14,9 @@
     (define-key shell-mode-map (kbd "C-l") 'comint-clear-buffer)
     (define-key shell-mode-map (kbd "C-r") 'comint-history-isearch-backward)
     (define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
-    (define-key shell-mode-map (kbd "C-n") 'comint-next-input))
+    (define-key shell-mode-map (kbd "C-n") 'comint-next-input)
+    ;; 空白行を出力しないようにする
+    (setq comint-use-prompt-regexp nil)
+    (setq comint-input-ignoredups t)
+    (setq comint-process-echoes t))  ;; コマンドをエコーしない
   (add-hook 'shell-mode-hook 'my/shell-mode-setup))
