c9b58d27e8165c1f59af80c55806c9ad1ad4f008
unixadmin <y.ochiai@x86-unix.com>
2024-12-31 07:02:29 +0900

    update

--- linux-init.el
+++ linux-init.el
@@ -2,13 +2,12 @@
 ; fonts install
 (defun install-hack-font ()
   "Download and install the Hack font."
-  (let* ((version "v3.003")  ;; バージョンをここで定義
+  (let* ((version "v3.003")  ;; バージョン定義
          (font-url (format "https://github.com/source-foundry/Hack/releases/download/%s/Hack-%s-ttf.zip" version version))
          (download-dir "/tmp")
          (zip-file (expand-file-name "Hack.zip" download-dir))
          (extract-dir (expand-file-name "Hack" download-dir))
          (font-dir (expand-file-name "~/.local/share/fonts")))
-
     ;; フォントファイルのダウンロード
     (url-copy-file font-url zip-file t)
 
@@ -18,18 +17,25 @@
         (make-directory extract-dir))
       (call-process "unzip" nil nil nil zip-file "-d" extract-dir))
 
-    ;; フォントファイルをコピー
-    (dolist (font-file (directory-files extract-dir t "\\.ttf$"))
-      (copy-file font-file font-dir t))
+    ;; フォントファイルを再帰的に検索してコピー
+    (let ((found-ttf-files nil))
+      (dolist (dir (list extract-dir))
+        (setq found-ttf-files
+              (append found-ttf-files
+                      (directory-files dir t "\\.ttf$" t)))
+        ;; サブディレクトリを再帰的に探索
+        (dolist (subdir (directory-files dir t "[^.]"))
+          (when (file-directory-p subdir)
+            (setq found-ttf-files
+                  (append found-ttf-files
+                          (directory-files subdir t "\\.ttf$" t)))))))
+
+      ;; 見つかったフォントファイルをコピー
+      (dolist (font-file found-ttf-files)
+        (copy-file font-file font-dir t)))
 
     ;; フォントキャッシュの更新
     (shell-command "fc-cache -f -v")))
-; フォントをインストールする関数を呼び出す
-(install-hack-font)
-
-; font setting
-(set-frame-font "Hack")
-(add-to-list 'default-frame-alist '(font . "Hack-12")
 
 ;; shell
 ; shell-pop
